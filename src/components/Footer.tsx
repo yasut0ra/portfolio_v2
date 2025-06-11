5b180c5a6117c265a85754b8d025f863e3df2ccf
やすとら <yastar.tkm83@gmail.com>
2025-06-11 14:31:24 +0900

    Fix missing X import in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Github as GitHub, Linkedin, Mail, ArrowUp } from 'lucide-react';
+import { Github as GitHub, Linkedin, Mail, ArrowUp, X } from 'lucide-react';
 
 const Footer: React.FC = () => {
   const scrollToTop = () => {
