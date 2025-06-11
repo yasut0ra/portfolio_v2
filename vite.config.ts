6a20245d43451f0b85e3a14f636d2bc11b13eda4
やすとら <yastar.tkm83@gmail.com>
2025-06-11 23:21:11 +0900

    GitHub Pages SPA routing fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio_v2/',
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
